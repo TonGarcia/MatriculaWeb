eaeddfcf18890d37ba2ad82ce64e5e20e43f5652
Ilton Silveira <iltonsilveira91@gmail.com>
2019-12-31 10:14:12 -0300

    subject updated

--- db/migrate/20191225213916_create_schools.rb
+++ db/migrate/20191225213916_create_schools.rb
@@ -1,8 +1,8 @@
 class CreateSchools < ActiveRecord::Migration[5.1]
   def change
     create_table :schools do |t|
-      t.string :name, null: false
-      t.string :code, null: false
+      t.string :name, null: false, unique: true
+      t.string :code, null: false, unique: true
       t.belongs_to :user, null: false, default: 1
 
       t.timestamps
