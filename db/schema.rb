6d741db6d18e6a348c5d4bdb33312a3c76851bdf
Ilton Silveira <iltonsilveira91@gmail.com>
2020-02-02 19:58:39 -0300

    initial user role restrictions added & removed IDE folder

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20191225213925) do
     t.string "timezone", default: "-03:00", null: false
     t.integer "role_id"
     t.string "gov_id", null: false
+    t.string "enrollment_id", default: ""
     t.string "phone_number", default: ""
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
